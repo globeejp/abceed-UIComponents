f1694633e1cbe225811976079e12c63c0536dc05
toshi0383 <t.suzuki326@gmail.com>
2023-09-04 22:10:09 +0900

    chore: Update Sample

---
+++ SampleApp/Book.swift
@@ -0,0 +1,14 @@
+import AbceedUILibrary
+
+extension Book {
+
+    static func kinfure() -> Book {
+        Book(
+            author: "TEX 加藤",
+            id_book: "tokyu_kinhure_new",
+            img_url: "https://d3grtcc7imzgqe.cloudfront.net/img/book_small/tokyu_kinhure_new.jpg",
+            name_book: "TOEIC L&R TEST 出る単特急 金のフレーズ",
+            publisher: "朝日新聞出版"
+        )
+    }
+}
